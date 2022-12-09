b42f3710c6cff369a1acbd8646f18b5360aa928a
proto <proto@LAPTOP-TND8NS36.education.lakanal.epf.fr>
2022-12-09 12:19:40 +0100

    création classe partie

---
+++ SuperDémineur/src/superdémineur/PartieD.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package superdémineur;
+
+/**
+ *
+ * @author proto
+ */
+public class PartieD {
+    
+    private Plateau2Jeu plateau;
+    private Joueur joueurCourant;
+    
+    
+    
+    
+    
+}
