9c66a2c60a5fb63545a8706f04a015025cfdabfa
Dell <Dell@DESKTOP-VJLVIIN>
2023-01-02 12:22:03 +0100

    interphace graphique

---
+++ SuperDémineur/src/superdémineur/CelluleGraphique.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package superdémineur;
+
+import javax.swing.JButton;
+
+/**
+ *
+ * @author Dell
+ */
+public class CelluleGraphique extends JButton {
+    CelluleDeGrille celluleAssociees;
+}
